7c6d1674e3a09d7d2b9b799c02c37e23fc86e263
Metythorn Penn <metythorn@gmail.com>
2023-10-17 15:19:23 +0700

    added enter button for enter data

--- main.py
+++ main.py
@@ -2,10 +2,66 @@ import tkinter
 from tkinter import messagebox, filedialog
 from tkinter import ttk
 from PIL import Image, ImageTk
-import os
+import os, sys
 
+
+# Create a font style with increased font size and bold
+custom_font = ("Courier New", 20, "bold")
+
+# Function to restart the software
+def restart_software():
+    python = sys.executable
+    os.execl(python, python, * sys.argv)
+    
+# Function to toggle between light and dark mode
+def toggle_dark_mode():
+    background_color = "#1E1E1E"
+    text_color = "white"
+    text_bt_color = "#1E1E1E"
+    window.configure(bg=background_color)
+    label_frame.configure(bg=background_color, fg=text_color)
+    total_images_label.configure(bg=background_color, fg=text_color)
+    image_name_label.configure(bg=background_color, fg=text_color)
+    text_label.configure(bg=background_color, fg=text_bt_color)
+    text_entry.configure(bg=background_color, fg=text_bt_color)
+    button_select_image.configure(bg=background_color, fg=text_bt_color)
+    button_enter_data.configure(bg=background_color, fg=text_bt_color)
+    button_next_image.configure(bg=background_color, fg=text_bt_color)
+    window.update_idletasks()
+    
+    
 window = tkinter.Tk()
 window.title("Data Entry Application | Optical Charactor Regonition")
+# window.configure(bg="#1E1E1E")  # Set background color for dark mode
+
+# -------------------------------- Custom Menu Bar ----------------------------
+# Create a menu bar
+menu_bar = tkinter.Menu(window)
+window.config(menu=menu_bar)
+
+# Create a File menu
+file_menu = tkinter.Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="File", menu=file_menu)
+file_menu.add_command(label="Restart", command=restart_software)  # Added Restart option
+file_menu.add_separator()
+file_menu.add_command(label="Exit", command=window.quit)
+
+# Create a View menu
+view_menu = tkinter.Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="View", menu=view_menu)
+view_menu.add_command(label="Toggle Dark Mode", command=toggle_dark_mode)
+view_menu.add_separator()
+view_menu.add_checkbutton(label="Show Image Names", onvalue=True, offvalue=False, variable=tkinter.BooleanVar())
+
+# Create a Help menu
+help_menu = tkinter.Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="Help", menu=help_menu)
+help_menu.add_command(label="About", command=lambda: messagebox.showinfo("About", "The purpose of this appplication is use for entry data of project OCR only!!!"))
+help_menu.add_separator()
+help_menu.add_command(label="Help", command=lambda: messagebox.showinfo("Help", "Any error or bugs please contact: Metythorn Penn | 19059"))
+
+# ------------------------- Window Size ------------------------------
+
 
 def update_label_frame_position(event):
     label_frame.update_idletasks()  # Update the label_frame geometry information
@@ -35,9 +91,13 @@ window.geometry(f"{window_width}x{window_height}+{x_coordinate}+{y_coordinate}")
 window.grid_rowconfigure(0, weight=1)
 window.grid_columnconfigure(0, weight=1)
 
+# ---------------------- || ---------------------------------
+
 # Variable to store the total number of images
 total_images = tkinter.StringVar()
 total_images.set("Total Images: 0")
+
+
 
 # Variable to track the current image index
 current_image_index = 0
@@ -47,7 +107,7 @@ image_files = []
 folder_path = ""
 
 
-# --------------- Action function when user clicks enter ----------
+# --------------------------- Action function when user clicks enter ----------------------------------
 # Function to handle entering data
 def enter_data():
     label = text_entry.get()
@@ -55,6 +115,7 @@ def enter_data():
     text_entry.focus()
 
     global current_image_index
+
     current_image_index = (current_image_index + 1) % len(image_files)
     if current_image_index == 0:
         messagebox.showinfo("End of Images", "You have reached the last image in the folder.")
@@ -132,7 +193,7 @@ def select_image_folder():
     global folder_path, image_files, current_image_index
     folder_path = filedialog.askdirectory()
     print(folder_path)
-    messagebox.showinfo("Image Folder", "You have selected " + folder_path)
+    # messagebox.showinfo("Image Folder", "You have selected " + folder_path)
     
     # save image folder path to text file
     with open("data_file.txt", "a") as file:
@@ -142,12 +203,11 @@ def select_image_folder():
     files = os.listdir(folder_path)
     image_files = [file for file in files if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp'))]
     
-    total_images.set("Total Images: " + str(current_image_index + 1) + " / " +str(len(image_files)))  # Update the total image count
+    # total_images.set("Total Images: " + str(current_image_index + 1) + " / " +str(len(image_files)))  # Update the total image count
+    total_images.set("Total Images: " +  str(len(image_files)))
+    total_images_label.config(font=custom_font)
+    
     # current_image_index.set("Current Image Index: " + str(current_image_index))
-    
-
-    
-    
     if image_files:
         current_image_index = 0
         update_image()
@@ -155,55 +215,62 @@ def select_image_folder():
         messagebox.showwarning("No Images", "The selected folder does not contain any images.")
 
 
-
 # ---------------- Button to enter data ---------------------
-label_frame = tkinter.LabelFrame(window, text="Enter Data Area")
+
+
+label_frame = tkinter.LabelFrame(window, text="Enter Data Area", font=custom_font)
 label_frame.grid(row=5, column=0, padx=10, pady=10, sticky="w")
 
 # Select Folder 
-button_select_image = tkinter.Button(label_frame, text="Select Image Folder", command=select_image_folder)
+button_select_image = tkinter.Button(label_frame, text="Select Image Folder", command=select_image_folder, font=custom_font)
 button_select_image.grid(row=0, column=0, padx=20, pady=2, sticky="w")
 
 # Label to display the total number of images
-total_images_label = tkinter.Label(label_frame, textvariable=total_images)
+total_images_label = tkinter.Label(label_frame, textvariable=total_images, font=custom_font)
 total_images_label.grid(row=1, column=0, padx=20, pady=2, sticky="w")
 
 # Label to display the image name
-image_name_label = tkinter.Label(label_frame, text="Image : ")
+image_name_label = tkinter.Label(label_frame, text="Image : ", font=custom_font)
 image_name_label.grid(row=1, column=1, padx=20, pady=2, sticky="w")
-
-
-# previous_image_label = tkinter.Label(label_frame, textvariable= current_image_index)
-# previous_image_label.grid(row=3, column=0, padx=20, pady=2, sticky="w")
-
 
 # Label to display the selected image
 image_label = tkinter.Label(label_frame)
 image_label.grid(row=2, column=0, padx=20, pady=2, sticky="w")
 
-text_label = tkinter.Label(label_frame, text="Label")
+text_label = tkinter.Label(label_frame, text="Label", font=custom_font)
 text_label.grid(row=3, column=0, columnspan=2, padx=20, pady=5, sticky="w")
 
 text_entry = tkinter.Entry(label_frame)
 text_entry.grid(row=3, column=0, columnspan=2, padx=25, pady=5, sticky="w")
 
-button_enter_data = tkinter.Button(label_frame, text="Enter Data", command=enter_data)
+button_enter_data = tkinter.Button(label_frame, text="Enter Data", command=enter_data, font= custom_font)
 button_enter_data.grid(row=3, column=1, columnspan=2, padx=20, pady=5, sticky="w")
 
 # Button to navigate to the next image
-button_next_image = tkinter.Button(label_frame, text="Previous Image", command=previous_image)
+button_next_image = tkinter.Button(label_frame, text="Previous Image", command=previous_image, font=custom_font)
 button_next_image.grid(row=4, column=0, padx=20, pady=5, sticky="w")
 
 
-
-
 for widget in label_frame.winfo_children():
-    widget.grid_configure(padx=100, pady=30, sticky="w")
+    widget.grid_configure(padx=150, pady=40, sticky="w")
+
+# --------------- Custom Shortcut Key -------------------
+# Bind the Enter key to call enter_data function
+def enter_data_key(event):
+    enter_data()
+    
+window.bind("<Return>", enter_data_key)
+
+
+# Bind the Up arrow key to call previous_image function
+def previous_image_key(event):
+    if event.state == 4:
+        previous_image()
+window.bind("<Left>", previous_image_key)
 
 # Bind the function to the window's resize event
 window.bind("<Configure>", update_label_frame_position)
 
 window.mainloop()
-
 
 
